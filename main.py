77ac4831c551095b57b6bbae4e61f6c293dbb74d
alexandre_bone <alexandre.bone@icm-institute.org>
2018-01-20 19:37:05 +0100

    Cut into patches.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 from PIL import Image
+from sklearn import feature_extraction
+
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
@@ -10,53 +12,78 @@ from keras.layers import Dense
 
 def load_image(image_path):
     image = Image.open(image_path)
-    return np.array(image)
+    return np.array(image).astype('float64')
+
 
 def load_train_database():
     path_to_train_database = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'training')
 
     path_to_images = os.path.join(path_to_train_database, 'images')
-    images = [load_image(os.path.join(path_to_images, path)) for path in os.listdir(path_to_images) if path[-3:] == 'tif']
+    images = [load_image(os.path.join(path_to_images, path)) for path in os.listdir(path_to_images) if
+              path[-3:] == 'tif']
 
     path_to_masks = os.path.join(path_to_train_database, 'mask')
     masks = [load_image(os.path.join(path_to_masks, path)) for path in os.listdir(path_to_masks) if path[-3:] == 'gif']
 
     path_to_targets = os.path.join(path_to_train_database, '1st_manual')
-    targets = [load_image(os.path.join(path_to_targets, path)) for path in os.listdir(path_to_targets) if path[-3:] == 'gif']
+    targets = [load_image(os.path.join(path_to_targets, path)) for path in os.listdir(path_to_targets) if
+               path[-3:] == 'gif']
 
     return images, masks, targets
 
+
+def remove_mean(images):
+    mean = sum(images) / float(len(images))
+    return [image - mean for image in images]
+
+
+def cut_into_patches(images, targets, patch_size=100):
+
+    image_patches = feature_extraction.image.extract_patches(
+        images[0], patch_shape=(patch_size, patch_size, 3),
+        extraction_step=patch_size).reshape([-1, patch_size, patch_size, 3])
+    target_patches = feature_extraction.image.extract_patches(
+        targets[0], patch_shape=(patch_size, patch_size),
+        extraction_step=patch_size).reshape([-1, patch_size, patch_size])
+
+    for (image, target) in enumerate(zip(images[1:], targets[1:])):
+        np.append(image_patches, feature_extraction.image.extract_patches(image, patch_shape=(patch_size, patch_size, 3), extraction_step=patch_size).reshape([-1, patch_size, patch_size, 3]), axis=0)
+        np.append(target_patches, feature_extraction.image.extract_patches(target, patch_shape=(patch_size, patch_size), extraction_step=patch_size).reshape([-1, patch_size, patch_size]), axis=0)
+
+    return image_patches, target_patches
+
+
 images, masks, targets = load_train_database()
+images = remove_mean(images)
+image_patches, target_patches = cut_into_patches(images, targets)
 
 print('End')
 
-
 model = Sequential()
-model.add(Conv2D(64, (3, 3), input_shape = (224, 224, 3), activation = 'relu',stride= 1,padding='same'))
-model.add(MaxPooling2D(pool_size = (2, 2),stride=2))
-model.add(Conv2D(128, (3, 3), activation = 'relu',stride= 1,padding='same'))
-model.add(MaxPooling2D(pool_size = (2, 2),stride=2))
-model.add(Conv2D(256, (3, 3), activation = 'relu',stride= 1,padding='same'))
-model.add(Conv2D(256, (3, 3), activation = 'relu',stride= 1,padding='same'))
-model.add(MaxPooling2D(pool_size = (2, 2),stride=2))
+model.add(Conv2D(64, (3, 3), input_shape=(224, 224, 3), activation='relu', stride=1, padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), stride=2))
+model.add(Conv2D(128, (3, 3), activation='relu', stride=1, padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), stride=2))
+model.add(Conv2D(256, (3, 3), activation='relu', stride=1, padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', stride=1, padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), stride=2))
 
-model.add(Conv2D(512, (3, 3),  activation = 'relu',stride= 1,padding='same'))
-model.add(Conv2D(512, (3, 3),  activation = 'relu',stride= 1,padding='same'))
-model.add(MaxPooling2D(pool_size = (2, 2),stride=2))
-model.add(Conv2D(512, (3, 3),  activation = 'relu',stride= 1,padding='same'))
-model.add(Conv2D(512, (3, 3),  activation = 'relu',stride= 1,padding='same'))
-model.add(MaxPooling2D(pool_size = (2, 2),stride=2))
+model.add(Conv2D(512, (3, 3), activation='relu', stride=1, padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu', stride=1, padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), stride=2))
+model.add(Conv2D(512, (3, 3), activation='relu', stride=1, padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu', stride=1, padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), stride=2))
 
 model.add(Flatten())
-model.add(Dense(units = 4096, activation = 'relu'))
-model.add(Dense(units = 4096, activation = 'relu'))
-model.add(Dense(units = 329960, activation = 'relu'))
+model.add(Dense(units=4096, activation='relu'))
+model.add(Dense(units=4096, activation='relu'))
+model.add(Dense(units=329960, activation='relu'))
 model.add(Dense(329960, activation='softmax'))
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 model.fit_generator(training_set,
-steps_per_epoch = 8000,
-epochs = 25,
-validation_data = test_set,
-validation_steps = 2000)
+                    steps_per_epoch=8000,
+                    epochs=25,
+                    validation_data=test_set,
+                    validation_steps=2000)
